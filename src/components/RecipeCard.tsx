db655fe67dba95937f2d84d047792f8713d83e27
Enitan Odupitan <odupitanenitan@gmail.com>
2023-12-20 11:21:46 +0200

    implements recipe card component

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
-import { Card, CardHeader, CardBody, CardFooter } from '@nextui-org/react'
-const RecipeCard = () => {
+import { Card, CardHeader, CardBody, CardFooter, Button } from '@nextui-org/react'
+import Image from 'next/image'
+
+interface RecipeCardProps {
+  image: string;
+  title: string;
+}
+const RecipeCard = ({image, title}: RecipeCardProps) => {
   return (
-    <div>RecipeCard</div>
+    <div>
+      <Card isFooterBlurred radius='sm' className='border-none w-[250px]'>
+        <Image 
+            src={image}
+            alt='continental_cuisine'
+            height={200}
+            width={250}
+            className=''
+        />
+        <CardFooter className='justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10'>
+          <p className='text-sm text-white/80'>{title}</p>
+          <Button className='text-tiny text-white bg-black/20' variant='flat' color='default' radius='lg' size='sm'>
+            See More
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
   )
 }
 
