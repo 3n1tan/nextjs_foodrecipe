ebea5a3e9f6fc4063652470772ce657b3ddc69e8
Enitan Odupitan <odupitanenitan@gmail.com>
2023-12-27 19:56:56 +0200

    refactoring code

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { Divider } from '@nextui-org/react';
-import React from 'react'
+import React, { Fragment } from 'react'
 import { FC } from 'react'
-
+import { Card, CardHeader, CardBody, CardFooter } from '@nextui-org/react';
 interface RecipeDetailsPage {
   params: {
     id: string
@@ -20,67 +20,55 @@ async function fetchData(params: {id: string}) {
 }
 const recipeDetailsPage: FC<RecipeDetailsPage> = async ({params}) => {
   let recipeDetailData = await fetchData(params)
-  console.log(params.id)
-  console.log(recipeDetailData.author)
+  // console.log(params.id)
+  // console.log(recipeDetailData.author)
+  const date = new Date(recipeDetailData.createdAt) ;
 
   return (
-    <div>
-        This page is for recipeDetailPage
-        <div className='max-w-xl ml-9'>
-          <div>
-            <img src="" alt="" />
-          </div>
-
-          <h1 className='text-[3rem] tracking-widest'>{recipeDetailData.name}</h1>
-        <div className="space-y-1">
-          <h4 className="text-medium font-medium">NextUI Components</h4>
-          <p className="text-small text-default-400">Beautiful, fast and modern React UI library.</p>
-        </div>
-          <Divider className="my-4" />
-        <div className="flex h-5 items-center space-x-4 text-small">
-          <div>
-            <h2>Blog</h2>
-            <ul>
-    
-            </ul>
-            
-          </div>
-            <Divider orientation="vertical" />
-          <div>Docs</div>
-            <Divider orientation="vertical" />
-          <div>Source</div>
-        </div>
-
-          {/* <div className='grid grid-cols-2'>
+    <Fragment>
+      <div className='flex justify-center'>   
+          <Card className='max-w-xl mt-9 pb-9 rounded-xl shadow-2xl'>
             <div>
-              <h2 className='uppercase'>Ingredients</h2>
-              <Divider/>
-              <ol>
-                <li>Hello</li>
-                <li>dfdvdv</li>
-                <li>dg dggv</li>
-                <li>bcffcc</li>
-                <li>dvdv v</li>
-                <li>dfdvv</li>
-                <li>v cc</li>
-              </ol>
-            </div>
-            <div>
-              <h2 className='uppercase'>Directions</h2>
-              <ol>
-                <li>Hello</li>
-                <li>dfdvdv</li>
-                <li>dg dggv</li>
-                <li>bcffcc</li>
-                <li>dvdv v</li>
-                <li>dfdvv</li>
-                <li>v cc</li>
-              </ol>
-            </div>
-          </div> */}
+              <div className='h-auto'>
+                <img src={recipeDetailData.imageURL} alt="" className='rounded-t-xl'/>
+              </div>
+              <h1 className='text-[2.5rem] tracking-wider uppercase text-center'>{recipeDetailData.name}</h1>
+              <div className="space-y-1 pl-5">
+                <div className='flex items-center space-x-4 h-5'>
+                  <div>
+                    <h4 className="font-medium text-small text-default-400">Origin: {recipeDetailData.origin}</h4>
+                  </div>
+                  <Divider orientation='vertical'/>
+                  <div>
+                    <h4 className="font-medium text-small text-default-400">Author: {recipeDetailData.author}</h4>
+                  </div>
+                  <Divider orientation='vertical'/>
+                  <div>
+                    <h4 className="font-medium text-small text-default-400">Date Added: {date.toDateString()}</h4>
+                  </div>
+                </div>
+                    <p className="text-small text-default-400">Beautiful, fast and modern React UI library.</p>
+              </div>
+              <Divider className="my-4" />
+              <div className="flex h-[100px] items-center space-x-4 text-small">
+                <div>
+                  <h2>Blog</h2>
+                  <ul>
 
-        </div>
-    </div>
+                  </ul>
+                  
+                </div>
+                  <Divider orientation="vertical" />
+                <div>Docs</div>
+                  <Divider orientation="vertical" />
+                <div>Source</div>
+              </div>
+
+            </div>
+          </Card>
+      </div>
+
+    </Fragment>
   )
 }
 
